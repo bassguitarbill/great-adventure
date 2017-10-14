4ddd183c275874f0e97e12db85a731d041cf8e92
Bill <bassguitarbill@gmail.com>
2017-10-14 13:39:21 -0400

    Don't hardcode beats

--- script/app.js
+++ script/app.js
@@ -7,13 +7,13 @@ requirejs(['trackLoader', 'trackData'], function(trackLoader, trackData) {
 		window.AudioContext = window.AudioContext || window.webkitAudioContext;
 		var audioContext = new window.AudioContext();
 		var audioElement = tracks[0].element;
-			source = audioContext.createMediaElementSource(audioElement);
-			source.connect(audioContext.destination);
-			audioElement.play();
-			window.setInterval(() => {
-				console.log(
-					Math.floor(source.mediaElement.currentTime / 0.219375) % 2
-				);
-			}, 1000/60);
+		var source = audioContext.createMediaElementSource(audioElement);
+		source.connect(audioContext.destination);
+		audioElement.play();
+		window.setInterval(() => {
+			console.log(
+				Math.floor(source.mediaElement.currentTime / tracks[0].beat) % 2
+			);
+		}, 1000/60);
 	});
 });
